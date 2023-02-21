1c5a4acd82849ca7cca8700b23419f4df11d6cf4
priyankarpal <priyankarpalpapai2005@gmail.com>
2023-02-21 09:55:26 +0530

    Update: nav links added

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,19 @@
 function header() {
   return (
-    <header class="title-bar">
-      <h1>FREE-HIT</h1>
+    <header className="title-bar">
+      <h1>🔥 FREE-HIT</h1>
+      <ul className="nav-links">
+        <li>
+          <a href="https://github.com/JasonDsouza212/free-hit" target="_blank">
+            😎 GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/JasonDsouza212" target="_blank">
+            🐦 Twitter
+          </a>
+        </li>
+      </ul>
     </header>
   );
 }
