a0e5c0f46e7e159fdf0203f310a64605cc08eca9
priyankarpal <priyankarpalpapai2005@gmail.com>
2023-02-28 21:58:37 +0530

    Added new style

--- src/components/card.js
+++ src/components/card.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { products } from "../DB/product";
 
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function Card() {
   const [category, setCategory] = useState("all");
@@ -17,107 +17,99 @@ function Card() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <div className="filter-section">
-          <div class="button-wrapper">
-            <button
-              rel="noopener noreferrer"
-              class="background-button"
-              onClick={() => filterProduct("all")}
-              title="All"
-            ></button>
-          </div>
-          <div class="button-wrapper">
-            <button
-              rel="noopener noreferrer"
-              class="background-button"
-              onClick={() => filterProduct("remote")}
-              title="Remote Jobs"
-            ></button>
-          </div>
-          <div class="button-wrapper">
-            <button
-              rel="noopener noreferrer"
-              class="background-button"
-              onClick={() => filterProduct("resume")}
-              title="Resume Builder"
-            ></button>
-          </div>
-          <div class="button-wrapper">
-            <button
-              rel="noopener noreferrer"
-              class="background-button"
-              onClick={() => filterProduct("tweet")}
-              title="Tweet to Image"
-            ></button>
-          </div>
-          <div class="button-wrapper">
-            <button
-              rel="noopener noreferrer"
-              class="background-button"
-              onClick={() => filterProduct("code")}
-              title="Code to Image"
-            ></button>
-          </div>
-          <div class="button-wrapper">
-            <button
-              rel="noopener noreferrer"
-              class="background-button"
-              onClick={() => filterProduct("tools")}
-              title="Useful Tools"
-            ></button>
-          </div>
-          <div class="button-wrapper">
-            <button
-              rel="noopener noreferrer"
-              class="background-button"
-              onClick={() => filterProduct("ethical")}
-              title="Ethical Hacking"
-            ></button>
-          </div>
-          <div class="button-wrapper">
-            <button
-              rel="noopener noreferrer"
-              class="background-button"
-              onClick={() => filterProduct("web")}
-              title="Web. Dev."
-            ></button>
-          </div>
+          <button
+            rel="noopener noreferrer"
+            class="background-button"
+            onClick={() => filterProduct("all")}
+          >
+            😉 All
+          </button>
+
+          <button
+            rel="noopener noreferrer"
+            class="background-button"
+            onClick={() => filterProduct("remote")}
+          >
+            🤯 Remote Jobs
+          </button>
+
+          <button
+            rel="noopener noreferrer"
+            class="background-button"
+            onClick={() => filterProduct("resume")}
+          >
+            😎 Resume Builder
+          </button>
+
+          <button
+            rel="noopener noreferrer"
+            class="background-button"
+            onClick={() => filterProduct("tweet")}
+          >
+            🤩 Tweet to Image
+          </button>
+
+          <button
+            rel="noopener noreferrer"
+            class="background-button"
+            onClick={() => filterProduct("code")}
+          >
+            🦾 Code to Image
+          </button>
+
+          <button
+            rel="noopener noreferrer"
+            class="background-button"
+            onClick={() => filterProduct("ethical")}
+          >
+            😍 Ethical Hacking
+          </button>
+
+          <button
+            rel="noopener noreferrer"
+            class="background-button"
+            onClick={() => filterProduct("tools")}
+          >
+            🛠️ Useful Tools
+          </button>
         </div>
-        <div id="products">
-          <div class="container">
-            {products.data.map((product, index) => {
-              if (category === "all" || category === product.category) {
-                return (
-                  <div key={index}>
-                    <div class="card">
-                      <div class="box">
-                        <div class="content">
-                          <LazyLoadImage
-                            className="card-image"
-                            src={product.image}
-                            alt={product.productName}
-                            effect={'blur'}
-                          />
-                          <h3>{product.productName.toUpperCase()}</h3>
-                          <a
-                            href={product.link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <button>Website</button>
-                          </a>
-                        </div>
+      </header>
+      <div id="products">
+        <div class="container">
+          {products.data.map((product, index) => {
+            if (category === "all" || category === product.category) {
+              return (
+                <div key={index}>
+                  <div class="card">
+                    <div class="box">
+                      <div class="content">
+                        <img
+                          className="card-image"
+                          src={product.image}
+                          alt={product.productName}
+                        />
+                        <h3>{product.productName}</h3>
+                        <a
+                          href={product.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <button className="website-link-btn">
+                            ↗️ Website
+                          </button>
+                        </a>
                       </div>
                     </div>
                   </div>
-                );
-              }
-              return null;
-            })}
-          </div>
+                </div>
+              );
+            }
+            return null;
+          })}
         </div>
-      </header>
+      </div>
     </div>
   );
 }
