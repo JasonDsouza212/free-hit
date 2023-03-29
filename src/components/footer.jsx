7cf6dc1e6bd137000fc92eedfd3dd064a301315f
Deva <devaags999@gmail.com>
2023-03-30 01:57:32 +0530

    Added styling to index.css

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,28 +1,17 @@
 function footer() {
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        borderTop: "0.5px solid #ddd",
-        paddingTop: "20px",
-        paddingBottom: "20px",
-        textAlign: "center",
-        paddingLeft: "40px",
-        paddingRight: "40px",
-        fontSize: "20px",
-      }}
-    >
+    <div class="footer">
       <div>
         <p>
           Crafted with ❤️ by
           <a
             href="https://github.com/JasonDsouza212"
             target="_blank"
-            className="footer-link"
+            class="footer-link"
             rel="noreferrer"
           >
             {" "}
-            <span style={{ color: "#0092CC" }}>Jason Dsouza</span>
+            Jason Dsouza
           </a>
         </p>
       </div>
