97f9b3c67fe986a6f03d4401eded741ffe87d31c
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-04-01 21:34:26 +0530

    Updated the project to use Vite Js, removed unwanted dependencies

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN pnpm install
 
 COPY . .
 
-EXPOSE 3000
+EXPOSE 5173
 
-CMD ["pnpm", "start"]
+CMD ["pnpm", "dev"]
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,33 @@
-function header() {
-  return (
-    <header className="title-bar">
-      <h1>🔥 FREE-HIT</h1>
-      <ul className="nav-links">
-        <li>
-          <a
-            className="nav-link"
-            href="https://github.com/JasonDsouza212/free-hit"
-            target="_blank"
-            rel="noreferrer"
-          >
-            😎 GitHub
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/_Jason_Dsouza"
-            target="_blank"
-            rel="noreferrer"
-            className="nav-link"
-          >
-            🐦 Twitter
-          </a>
-        </li>
-      </ul>
-    </header>
-  )
+import React from "react"
+
+const Header = () => {
+    return (
+        <header className="title-bar">
+            <h1>🔥 FREE-HIT</h1>
+            <ul className="nav-links">
+                <li>
+                    <a
+                        className="nav-link"
+                        href="https://github.com/JasonDsouza212/free-hit"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        😎 GitHub
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://twitter.com/_Jason_Dsouza"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="nav-link"
+                    >
+                        🐦 Twitter
+                    </a>
+                </li>
+            </ul>
+        </header>
+    )
 }
 
-export default header
+export default Header
