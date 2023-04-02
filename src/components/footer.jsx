b627627f483d6040700b275a9cd6402a2cec5a5c
Deva <devaags999@gmail.com>
2023-04-02 15:46:19 +0530

    Modified footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,23 +1,24 @@
-import React from "react"
+import React from 'react'
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="wrapper">
-                <p>
-                    Crafted with ❤️ by
-                    <a
-                        href="https://github.com/JasonDsouza212"
-                        target="_blank"
-                        className="footer-link"
-                        rel="noreferrer"
-                    >
-                        {''} Jason Dsouza
-                    </a>
-                </p>
-            </div>
-        </footer>
-    )
+  return (
+    <div className="footer">
+      <div>
+        <p>
+          Crafted with ❤️ by
+          <a
+            href="https://github.com/JasonDsouza212"
+            target="_blank"
+            className="footer-link"
+            rel="noreferrer"
+          >
+            {' '}
+            Jason Dsouza
+          </a>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
