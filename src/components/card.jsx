045d330d0e847be250225c92c7446d27aa6bad0c
Jason <jasondsouza212@gmail.com>
2023-04-06 22:26:02 +0530

    search bar and logo is fixed

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,166 +1,132 @@
-import React, { useState, useEffect } from 'react'
-import products from '../DB/product.json'
-
-const Card = () => {
-    const [category, setCategory] = useState('all')
-
-    function filterProduct(value) {
-        setCategory(value);
-        filteredButtonSelected(value);
-    }
-
-
-    async function filteredButtonSelected(value) {
-        const button = document.querySelectorAll('.category-select');
-        // Remove the "background-button-selected" class everytime the button is clicked at start to clear old selection
-        button.forEach((i) => {
-            i.classList.remove('background-button-selected');
-        });
-        let cnt = -1;
-        // Add the "background-button-selected" class to individual the button when it is clicked
-        button.forEach((i) => {
-            let selected = '';
-            selected = i.getAttribute('productcategory');
-            cnt++;
-            if (value === selected) {
-                button[cnt].classList.add('background-button-selected');
-                return;
-            }
-        });
-    }
-
-
-    useEffect(() => {
-        setCategory('all')
-    }, [])
-
+const Card = ({ filterProduct, filteredProducts, searchTerm ,setSearchTerm, category}) => {
     return (
-        <div className="App">
-            <header>
-                <div className="filter-section">
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="all"
-                        onClick={() => filterProduct("all")}
-                    >
-                        😉 All
-                    </button>
-
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="remote"
-                        onClick={() => filterProduct("remote")}
-                    >
-                        🤯 Remote Jobs
-                    </button>
-
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="resume"
-                        onClick={() => filterProduct("resume")}
-                    >
-                        😎 Resume Builder
-                    </button>
-
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="tweet"
-                        onClick={() => filterProduct("tweet")}
-                    >
-                        🤩 Tweet to Image
-                    </button>
-
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="ai"
-                        onClick={() => filterProduct("ai")}
-                    >
-                        🤖 AI
-                    </button>
-
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="ethical"
-                        onClick={() => filterProduct("ethical")}
-                    >
-                        😍 Ethical Hacking
-                    </button>
-
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="movies"
-                        onClick={() => filterProduct("movies")}
-                    >
-                        🎥 Movies | Series
-                    </button>
-
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="extensions"
-                        onClick={() => filterProduct("extensions")}
-                    >
-                        🔌 Useful Extensions
-                    </button>
-
-                    <button
-                        rel="noopener noreferrer"
-                        className="category-select background-button"
-                        productcategory="tools"
-                        onClick={() => filterProduct("tools")}
-                    >
-                        🛠️ Useful Tools
-                    </button>
+      <div className="App">
+        <header>
+          <div className="filter-section">
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="all"
+              onClick={() => filterProduct("all")}
+            >
+              😉 All
+            </button>
+  
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="remote"
+              onClick={() => filterProduct("remote")}
+            >
+              🤯 Remote Jobs
+            </button>
+  
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="resume"
+              onClick={() => filterProduct("resume")}
+            >
+              😎 Resume Builder
+            </button>
+  
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="tweet"
+              onClick={() => filterProduct("tweet")}
+            >
+              🤩 Tweet to Image
+            </button>
+  
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="ai"
+              onClick={() => filterProduct("ai")}
+            >
+              🤖 AI
+            </button>
+  
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="ethical"
+              onClick={() => filterProduct("ethical")}
+            >
+              😍 Ethical Hacking
+            </button>
+  
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="movies"
+              onClick={() => filterProduct("movies")}
+            >
+              🎥 Movies | Series
+            </button>
+  
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="extensions"
+              onClick={() => filterProduct("extensions")}
+            >
+              🔌 Useful Extensions
+            </button>
+  
+            <button
+              rel="noopener noreferrer"
+              className="category-select background-button"
+              productcategory="tools"
+              onClick={() => filterProduct("tools")}
+            >
+              🛠️ Useful Tools
+            </button>
+          </div>
+        </header>
+        <div className="card_wrapper">
+          <div className="cols">
+            {filteredProducts.map((product, index) => {
+              return category === 'all' || category === product.category ? (
+                <div className="col" key={index}>
+                  <div className="container">
+                    <div className="front">
+                      <div className="inner">
+                        <span>
+                          <img
+                            className="card_image"
+                            src={product.image}
+                            alt={product.productName}
+                          />
+                        </span>
+                        <p>{product.productName}</p>
+                      </div>
+                    </div>
+                    <div className="back">
+                      <div className="inner">
+                        <h3>{product.productName}</h3>
+                        <br />
+                        <p>{product.description}</p>
+                        <br />
+                        <a
+                          href={product.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Visit Website
+                        </a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            </header>
-            <div className="card_wrapper">
-                <div className="cols">
-                    {products.map((product, index) => {
-                        return category === 'all' || category === product.category ? (
-                            <div className="col" key={index}>
-                                <div className="container">
-                                    <div className="front">
-                                        <div className="inner">
-                                            <span>
-                                                <img
-                                                    className="card_image"
-                                                    src={product.image}
-                                                    alt={product.productName}
-                                                />
-                                            </span>
-                                            <p>{product.productName}</p>
-                                        </div>
-                                    </div>
-                                    <div className="back">
-                                        <div className="inner">
-                                            <h3>{product.productName}</h3>
-                                            <br />
-                                            <p>{product.description}</p>
-                                            <br />
-                                            <a
-                                                href={product.link}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                Visit Website
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ) : null
-                    })}
-                </div>
-            </div>
+              ) : null;
+            })}
+          </div>
         </div>
-    )
-}
-
-export default Card
+      </div>
+    );
+  };
+  
+  export default Card;
+  
