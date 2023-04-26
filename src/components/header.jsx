130f69a9c3ca8455910949b16b23e69d0029ab11
ahtasham <ahtaxam03@gmail.com>
2023-04-26 23:52:07 +0500

    style: refactor code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import freehitlogo from '../images/freehitLogo.png'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 const Header = ({ searchTerm, setSearchTerm }) => {
   return (
     <header className="title-bar" name="Top">
-      <h1>
+      <div className='logo-name'>
         <img className="logo" src={freehitlogo} alt="My Image" />
-        FREE-HIT
-      </h1>
-      <ul className="hnav-links">
-        <li>
+        <span className='name'>FREE-HIT</span> 
+        
+      </div>
+      <div className="hnav-links">
           <input
             type="text"
             placeholder=" Search for a tool or category..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-        </li>
-      </ul>
+          <AiOutlineSearch className='search-icon'/>
+      </div>
     </header>
   )
 }
