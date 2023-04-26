091b9f312f4f04ff14c6d9f1321be794f13b6fcd
AhorIsaac <ahorisaac.ai@gmail.com>
2023-04-26 17:38:30 +0100

    test: Footer component contains a clikable link that correctly opens in a new tab

--- src/components/__tests__/Footer.test.jsx
+++ src/components/__tests__/Footer.test.jsx
@@ -26,4 +26,31 @@ describe('Footer', () => {
     const listElement = screen.getByRole('list')
     expect(listElement).toBeInTheDocument()
   })
+
+  test('contains a clikable link that correctly opens in a new tab', async () => {
+    const user = userEvent.setup()
+
+    render(<Footer />)
+
+    const linkElement = screen.getByRole('link', {
+      name: /Jason Dsouza/i,
+    })
+    expect(linkElement).toBeInTheDocument()
+    expect(linkElement.getAttribute('href')).toBe(
+      'https://github.com/JasonDsouza212/free-hit'
+    )
+
+    await act(() => user.click(linkElement))
+
+    /*
+    if the button is not disabled and has a href attribute
+    which is not '' or null then it will open the url in 
+    a new tab when the user clicks on it
+    */
+    expect(document.activeElement).toBe(linkElement)
+    expect(linkElement).not.toHaveAttribute('disabled')
+    expect(linkElement).toHaveAttribute('href')
+    expect(linkElement.getAttribute('href')).not.toBeNull()
+    expect(linkElement.getAttribute('href')).length.greaterThan(5)
+  })
 })
