479a228df6cabc5f09ef7faa221508f78b12d483
Harsh Panchal <95937722+HarshPanchal26@users.noreply.github.com>
2023-05-04 11:43:12 +0530

    Update header.jsx
    Add Code for hide suggestion box while selection any of the suggestion .

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,16 @@ import freehitlogo from '../images/freehitLogo.png'
 
 const Header = ({ searchTerm, setSearchTerm, filteredSuggestions }) => {
   const handleSuggestionClick = (value) => {
+    document.getElementById('serch-suggestions').classList.add('diplay-none');
     setSearchTerm(value)
+  }
+
+  const handleChageInInput = (event)=>{
+    setSearchTerm(event.target.value);
+    let targetElem = document.getElementById('serch-suggestions');
+    if( filteredSuggestions.length > 0 && targetElem.className.includes('diplay-none')){
+      targetElem.classList.remove('diplay-none');
+    }
   }
   return (
     <header className="title-bar" name="Top">
@@ -18,7 +27,7 @@ const Header = ({ searchTerm, setSearchTerm, filteredSuggestions }) => {
               type="text"
               placeholder=" Search for a tool or category..."
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={(e)=> handleChageInInput(e)}
             />
             {searchTerm.length > 0 && (
               <div
@@ -30,7 +39,7 @@ const Header = ({ searchTerm, setSearchTerm, filteredSuggestions }) => {
             )}
           </div>
           {filteredSuggestions.length > 0 && (
-            <ul className="hnav-suggestionbar">
+            <ul className="hnav-suggestionbar" id='serch-suggestions'>
               {/* This shows as a list of suggestions based on the search term
                */}
               {filteredSuggestions.map((suggestion) => (
