b76f867d4a990a72731fae1d182933522e2eb9ba
Abhaya <shankarabhaya9@gmail.com>
2023-05-23 16:04:21 +0530

    list design progress 40%

--- src/components/Card/ListView.jsx
+++ src/components/Card/ListView.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { ToolContext } from '../../App'
+import '../../styles/ListView.css'
 
 const ListView = () => {
   const {
@@ -10,9 +11,52 @@ const ListView = () => {
     deleteres,
   } = useContext(ToolContext)
   return (
-    <div>
-      <h3>List View</h3>
-    </div>
+    <main className="list">
+      {filteredProducts.map((product, index) => {
+        return category === 'all' || category === product.category ? (
+          <article key={index}>
+            <div className="image">
+              <img className="card-img" src={product.image} alt="" />
+            </div>
+            <div className="text">
+              <div>
+                <span className="card-title">{product.productName}</span>
+                <p>{product.description}</p>
+              </div>
+              <div className="btn-cont">
+                <a target="_blank" href={product.link}>
+                  <button className="visit">
+                    <font color="white" size="4">
+                      Visit
+                    </font>
+                  </button>
+                </a>
+                {bookmarkfilteredProducts.some(
+                  (obj) => obj['productName'] === product.productName
+                ) ? (
+                  <button onClick={() => deleteres(product)}>
+                    <a href="#">
+                      Delete<i className="ri-bookmark-fill"></i>
+                    </a>
+                  </button>
+                ) : (
+                  <a href="#">
+                    <button
+                      className="bookmark"
+                      onClick={() => handelBookmarkAdd(product)}
+                    >
+                      <font color="white" size="4">
+                        Bookmark
+                      </font>
+                    </button>
+                  </a>
+                )}
+              </div>
+            </div>
+          </article>
+        ) : null
+      })}
+    </main>
   )
 }
 
