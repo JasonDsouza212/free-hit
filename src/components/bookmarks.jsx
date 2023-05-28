79bb682e5955d8341a1d16a957e5bb7fb9670689
Pooranjoy <pooranjoyb2016@gmail.com>
2023-05-28 12:52:26 +0530

    Fixed major bug

--- src/components/bookmarks.jsx
+++ src/components/bookmarks.jsx
@@ -1,48 +1,75 @@
-import React, { useContext } from 'react'
-import Header from './header'
-import { ToolContext } from '../App'
+import React, { useContext, useEffect, useState } from 'react';
+import Header from './header';
+import { ToolContext } from '../App';
 
 const BookMarks = ({ length }) => {
+  const [flag, setFlag] = useState(false);
   const { category, bookmarkfilteredProducts, deleteres } =
-    useContext(ToolContext)
+    useContext(ToolContext);
+
+  useEffect(() => {
+    const hasBookmark = bookmarkfilteredProducts.some(
+      (product) => category === 'all' || category === product.category
+    );
+    setFlag(hasBookmark);
+  }, [category, bookmarkfilteredProducts]);
+
   return (
     <>
       <Header />
       <div className="card-container">
         {length === 0 ? (
-          <p className="no-results">Sorry, no BookMarks to Show</p>
+          category === 'all' ? (
+            <p className="no-results">Please bookmark something to begin</p>
+          ) : (
+            <p className="no-results">
+              There aren't bookmarks from {category} section
+            </p>
+          )
         ) : (
-          <main className="grid">
-            {bookmarkfilteredProducts.map((product) => {
-              return category === 'all' || category === product.category ? (
-                <article>
-                  <div className="text">
-                    <h3>
-                      <img className="card-img" src={product.image} alt="" />
-                      <span className="card-title">{product.productName}</span>
-                    </h3>
-                    <p>{product.description}</p>
-                    <div className="btn-cont">
-                      <button>
-                        <a target="_blank" href={product.link}>
-                          Visit
-                        </a>
-                      </button>
-                      <button onClick={() => deleteres(product)}>
-                        <a href="#">
-                          Delete <i className="ri-bookmark-fill"></i>
-                        </a>
-                      </button>
-                    </div>
-                  </div>
-                </article>
-              ) : null
-            })}
-          </main>
+          <>
+            {flag ? (
+              <main className="grid">
+                {bookmarkfilteredProducts.map((product) => {
+                  return (
+                    (category === 'all' || category === product.category) && (
+                      <article>
+                        <div className="text">
+                          <h3>
+                            <img className="card-img" src={product.image} alt="" />
+                            <span className="card-title">{product.productName}</span>
+                          </h3>
+                          <p>{product.description}</p>
+                          <div className="btn-cont">
+                            <button>
+                              <a target="_blank" href={product.link}>
+                                Visit
+                              </a>
+                            </button>
+                            <button onClick={() => deleteres(product)}>
+                              <a href="#">
+                                Delete <i className="ri-bookmark-fill"></i>
+                              </a>
+                            </button>
+                          </div>
+                        </div>
+                      </article>
+                    )
+                  );
+                })}
+              </main>
+            ) : (
+              <p className="no-results">
+                {category === 'all'
+                  ? 'There are no bookmarks'
+                  : `There aren't bookmarks from ${category} section`}
+              </p>
+            )}
+          </>
         )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default BookMarks
+export default BookMarks;
