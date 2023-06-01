7efc5201d6db7281e88fce6d0d2312fe8372e5f8
Abishek Upadhyay <abishekupadhyay2020@gmail.com>
2023-06-01 18:43:58 +0530

    error image change

--- src/components/Commonpage.jsx
+++ src/components/Commonpage.jsx
@@ -1,12 +1,23 @@
 import Header from './header'
-import errorimg from '../images/errorimg.png'
+import errorimg from '../images/errorImg2.jpg'
 import { useNavigate } from 'react-router-dom'
 const Commonpage = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate()
   return (
     <>
       <Header />
-        <img className='errorImage' src={errorimg} alt="error Image" onClick={()=>{navigate(-1)}}></img>
+      <div className='errorBox'>
+        <div></div>
+        <img
+          className="errorImage"
+          src={errorimg}
+          alt="error Image"
+          onClick={() => {
+            navigate(-1)
+          }}
+        ></img>
+        <div></div>
+      </div>
     </>
   )
 }
