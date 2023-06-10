d0d118cb55a7f0c7aa28a6abd3f3fa03ff7a3c39
Esha-01 <98593600+Esha-01@users.noreply.github.com>
2023-06-10 18:47:40 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,51 +1,19 @@
 // const Footer = () => {
 //   // gettin year dynamically
-//   const date = new Date();
-//   const year = date.getFullYear();
+
 //   return (
-//     <footer>
-//       <div className="footer-content">
-//         <h3>
-//           <b>FREE-HIT</b>
-//         </h3>
-//         <p>
-//           Find the tools you need without breaking the bank
-//           <br /> <b>Contact Us</b>
-//         </p>
-//         <ul className="socials">
-//           <li>
-//             <a target="_blank" href="https://twitter.com/_Jason_Dsouza">
-//               <i className="ri-twitter-fill"></i>
-//             </a>
-//           </li>
-//           <li>
-//             <a target="_blank" href="https://github.com/JasonDsouza212">
-//               <i className="ri-github-fill"></i>
+//    
 //             </a>
 //           </li>
 //           <li>
 //             <a
-//               target="_blank"
-//               href="https://www.linkedin.com/in/jason-dsouza-130b421ba/"
+//            
 //             >
-//               <i className="ri-linkedin-fill"></i>
+//             
 //             </a>
-//           </li>
-//           <li>
-//             <a target="_blank" href="mailto:jasondsouza212@gmail.com">
-//               <i className="ri-mail-fill"></i>
+//    
 //             </a>
-//           </li>
-//         </ul>
-//       </div>
-//       <div className="footer-bottom">
-//         <p>
-//           copyright &copy;{year} <b>Free-Hit</b>
-//           <br />
-//           Maintained by{' '}
-//           <span className="maintainer">
-//             <a target="_blank" href="https://github.com/JasonDsouza212">
-//               Jason Dsouza
+//         
 //             </a>
 //           </span>
 //         </p>
