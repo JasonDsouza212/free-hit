2114b58d38864d22979f78c3a1deba5f4529df62
Vidip Ghosh <vidip2001@gmail.com>
2023-06-13 22:42:12 +0530

    Improved Contributors Section

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -16,11 +16,12 @@ const Team = () => {
       <div className="team-content">
         <div className="box">
           <img src={con1} />
-          <h3>Abhishek K</h3>
+          <h4>Abhishek K</h4>
           <h5>Designer</h5>
+          <hr />
           <div className="icons">
             <a href="https://twitter.com/bobsstwt" target="_blank">
-              <i className="ri-twitter-fill"></i>
+              <i style={{color:'#1DA1F2'}} className="ri-twitter-fill"></i>
             </a>
             <a href="https://github.com/bobbyy16" target="_blank">
               <i className="ri-github-fill"></i>
@@ -29,35 +30,36 @@ const Team = () => {
               href="https://www.linkedin.com/in/abhishek-k-7597771ba/"
               target="_blank"
             >
-              <i className="ri-linkedin-fill"></i>
+              <i style={{color:'#0077B5'}} class="ri-linkedin-box-fill"></i>
             </a>
           </div>
         </div>
-
         <div className="box">
           <img src={con2} />
-          <h3>Priyankar</h3>
+          <h4>Priyankar</h4>
           <h5>Developer</h5>
+          <hr />
           <div className="icons">
             <a href="https://twitter.com/Priyankarpal" target="_blank">
-              <i className="ri-twitter-fill"></i>
+              <i style={{color:'#1DA1F2'}} className="ri-twitter-fill"></i>
             </a>
             <a href="https://github.com/priyankarpal" target="_blank">
               <i className="ri-github-fill"></i>
             </a>
             <a href="https://www.linkedin.com/in/priyankarpal/" target="_blank">
-              <i className="ri-linkedin-fill"></i>
+              <i style={{color:'#0077B5'}} class="ri-linkedin-box-fill"></i>
             </a>
           </div>
         </div>
 
         <div className="box">
           <img src={con3} />
-          <h3>Jason Dsouza</h3>
+          <h4>Jason Dsouza</h4>
           <h5>Developer</h5>
+          <hr />
           <div className="icons">
             <a href="https://twitter.com/_Jason_Dsouza" target="_blank">
-              <i className="ri-twitter-fill"></i>
+              <i style={{color:'#1DA1F2'}} className="ri-twitter-fill"></i>
             </a>
             <a href="http://github.com/JasonDsouza212" target="_blank">
               <i className="ri-github-fill"></i>
@@ -66,15 +68,16 @@ const Team = () => {
               href="https://www.linkedin.com/in/jason-dsouza-130b421ba/"
               target="_blank"
             >
-              <i className="ri-linkedin-fill"></i>
+              <i style={{color:'#0077B5'}} class="ri-linkedin-box-fill"></i>
             </a>
           </div>
         </div>
 
         <div className="box" onClick={() => navigate('/community')}>
           <img src={con4} />
-          <h3> Open-Source</h3>
+          <h4> Open-Source</h4>
           <h5>Community</h5>
+          <hr />
           <div className="icons">
             <a
               href="https://github.com/JasonDsouza212/free-hit/graphs/contributors"
