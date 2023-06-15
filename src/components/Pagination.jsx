b582cd468f09da91c5e158c7e6cbad08810ddfb8
kai <kailashjat604@gmail.com>
2023-06-15 23:08:29 +0530

    change style

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -16,7 +16,7 @@ export default function Pagination({ totalPages, atTop }) {
 	}
 	return (
 		atTop
-			? <div className="pagination-top">
+			? <div className="pagination-top top-pagination">
 				{currentPage > 1 && <button className="paginate-nav" onClick={() => handleClick(currentPage - 1)} > &lt;  </button>}
 				{
 					pageNumbers.slice(currentPage - 2 >= 0 ? currentPage - 2 : 0, currentPage + 1).map(number =>
