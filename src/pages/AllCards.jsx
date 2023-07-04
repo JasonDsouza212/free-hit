ef7d0aad455970ac1d16bb192236d4fbe367e266
Shreya Dhiman <shreyanksha2004@gmail.com>
2023-07-04 20:37:46 +0530

    home page redesigned

--- src/pages/AllCards.jsx
+++ src/pages/AllCards.jsx
@@ -1,29 +1,27 @@
 import Header from '../components/Navbar'
 import React, { useContext, useEffect, useState } from 'react'
 import { ToolContext } from '../App'
-import noresultimg from '../assets/sad-face-2.webp'
+import noresultimg from '../assets/sad-face-2.png'
 import GridView from '../components/Card/GridView'
 import ListView from '../components/Card/ListView'
 import { BsFillGridFill, BsListUl } from 'react-icons/bs'
-import { RiMoonClearFill, RiSunFill } from 'react-icons/ri' // Import the icons for dark mode toggle
 import '../styles/Home.css'
 import { useSearchParams, Navigate } from 'react-router-dom'
-import checkFilter from "..//utils/check_filters"
+import checkFilter from "../utils/check_filters"
 import Loader from '../components/Loader'
 import products from "../DB/product.json"
 import filterProducts from '../utils/filter/filter_products'
 import searchProducts from '../utils/search/search_products'
 import Pagination from '../components/Pagination'
 import FreeHit from '../components/FreeHit'
+import { useHistory } from 'react-router-dom';
 
-const Card = () => {
+
+const AllCard = () => {
   const {
     gridView,
     setGridView,
-       darkMode,
-    setDarkMode
   } = useContext(ToolContext)
-
 
   const [searchParams,] = useSearchParams()
   let filters = searchParams.get('filters') || "all"
@@ -45,7 +43,7 @@ const Card = () => {
 
   const productNames = filteredProducts?.map((product) => product.productName) || []
 
-  const productsPerPage = 8
+  const productsPerPage = 16
   const [visibleProducts, setVisibleProducts] = useState(currentProducts.slice(0, productsPerPage));
 
   const loadMoreProducts = () => {
@@ -68,25 +66,16 @@ const Card = () => {
     // currentProducts = currentProducts.slice(firstProductIndex, lastProductIndex)
   }
 
-    // darkmode function
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-
-    // Changing the nav bar theme during toggle
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', darkMode ? '#373530' : '#f1f1ef');
-  };
 
   return (
     <>
-    <div className={`card_container ${darkMode ? 'dark-mode' : ''}`}>
     <Header filteredSuggestions={filterNames} />
     <section>
-    <FreeHit/>
     <div><h1>Free-Hit's Tools!</h1></div>
     </section>
+    <div className="card_container">
       <div className="card_view">
       {totalPages > 1 && searchTerm.length == 0 && <Pagination totalPages={totalPages} atTop />}
-
         <BsFillGridFill
           onClick={() => setGridView(true)}
           size={22}
@@ -97,27 +86,15 @@ const Card = () => {
           size={28}
           color={gridView ? "#9E9E9E" : "#212121"}
         />
-          <button className={`dark-mode-toggle ${darkMode ? 'dark-mode' : ''}`} onClick={toggleDarkMode}>
-          {darkMode ? (
-            <span className="icon" role="img" aria-label="moon">
-              🌙
-            </span>
-          ) : (
-            <span className="icon" role="img" aria-label="sun">
-              ☀️
-            </span>
-          )}
-        </button>
-
       </div>
       <div className="card-container">
         {!isLoading ?
-          currentProducts.length === 0 ? (
+          currentProducts.length == 0 ? (
             <div className="not-found-wrapper">
               <p className="no-results">
                 Sorry, our toolbox seems empty for this search term!
               </p>
-              <img className="not-found-img" src={noresultimg} alt="not found" />
+              <img class="not-found-img" src={noresultimg} alt="not found" />
             </div>
           ) : gridView ? (
             <GridView currentProducts={currentProducts} />
@@ -130,13 +107,10 @@ const Card = () => {
           </div>
         }
       </div>
-    {/* <NavLink to="/allCards">More</NavLink> */}
       {totalPages > 1 && searchTerm.length == 0 && <Pagination totalPages={totalPages} />}
-
     </div>
-
     </>
   )
 }
 
-export default Card
+export default AllCard
--- temp/Home.jsx
+++ temp/Home.jsx
@@ -5,7 +5,6 @@ import noresultimg from '../assets/sad-face-2.webp'
 import GridView from '../components/Card/GridView'
 import ListView from '../components/Card/ListView'
 import { BsFillGridFill, BsListUl } from 'react-icons/bs'
-import { RiMoonClearFill, RiSunFill } from 'react-icons/ri' // Import the icons for dark mode toggle
 import '../styles/Home.css'
 import { useSearchParams, Navigate } from 'react-router-dom'
 import checkFilter from "..//utils/check_filters"
@@ -20,10 +19,7 @@ const Card = () => {
   const {
     gridView,
     setGridView,
-       darkMode,
-    setDarkMode
   } = useContext(ToolContext)
-
 
   const [searchParams,] = useSearchParams()
   let filters = searchParams.get('filters') || "all"
@@ -68,22 +64,15 @@ const Card = () => {
     // currentProducts = currentProducts.slice(firstProductIndex, lastProductIndex)
   }
 
-    // darkmode function
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-
-    // Changing the nav bar theme during toggle
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', darkMode ? '#373530' : '#f1f1ef');
-  };
 
   return (
     <>
-    <div className={`card_container ${darkMode ? 'dark-mode' : ''}`}>
     <Header filteredSuggestions={filterNames} />
     <section>
     <FreeHit/>
     <div><h1>Free-Hit's Tools!</h1></div>
     </section>
+    <div className="card_container">
       <div className="card_view">
       {totalPages > 1 && searchTerm.length == 0 && <Pagination totalPages={totalPages} atTop />}
 
@@ -97,27 +86,15 @@ const Card = () => {
           size={28}
           color={gridView ? "#9E9E9E" : "#212121"}
         />
-          <button className={`dark-mode-toggle ${darkMode ? 'dark-mode' : ''}`} onClick={toggleDarkMode}>
-          {darkMode ? (
-            <span className="icon" role="img" aria-label="moon">
-              🌙
-            </span>
-          ) : (
-            <span className="icon" role="img" aria-label="sun">
-              ☀️
-            </span>
-          )}
-        </button>
-
       </div>
       <div className="card-container">
         {!isLoading ?
-          currentProducts.length === 0 ? (
+          currentProducts.length == 0 ? (
             <div className="not-found-wrapper">
               <p className="no-results">
                 Sorry, our toolbox seems empty for this search term!
               </p>
-              <img className="not-found-img" src={noresultimg} alt="not found" />
+              <img class="not-found-img" src={noresultimg} alt="not found" />
             </div>
           ) : gridView ? (
             <GridView currentProducts={currentProducts} />
@@ -130,11 +107,14 @@ const Card = () => {
           </div>
         }
       </div>
+
     {/* <NavLink to="/allCards">More</NavLink> */}
+      
       {totalPages > 1 && searchTerm.length == 0 && <Pagination totalPages={totalPages} />}
 
     </div>
 
+    
     </>
   )
 }
