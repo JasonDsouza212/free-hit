d517e9a2dd3003a78f11be2248cbcf0ff103c093
vivek <20bcs105@iiitdwd.ac.in>
2023-07-06 20:46:05 +0530

    added sideBar for the categories and updated logo

--- extenstion/script.js
+++ extenstion/script.js
@@ -1,3 +1,11 @@
+const inputBar = document.querySelector('.input')
+const results = document.querySelector('.results')
+// const themeButton = document.querySelector('#theme-btn')
+const categoryButton = document.querySelector("#category-btn")
+const categoryList = document.querySelector('.category-list')
+let websites;
+// themeButton.addEventListener('click',changeTheme)
+
 function result(productName,category,image,link,description){
     //result element to add html form of the website
     let ele = document.createElement('article')
@@ -39,66 +47,85 @@ function result(productName,category,image,link,description){
     return ele
 }
 
-
-const fetchWebsites = (results)=>{
- //the websites are taken from product.json
-    fetch('DB/product.json')
-  .then(response => response.json())
-  .then(data => {
-    websites = data
-    data.forEach(element => {
+    
+function changeTheme(e){
+    e.target.innerHTML = ""
+    if(document.querySelector('body').classList.contains('dark')){
+        document.querySelector('body').classList.remove('dark')
+        e.target.innerHTML = `<i class="fa-solid fa-sun"></i>`
+    }else{
+        document.querySelector('body').classList.add('dark')
+        e.target.innerHTML = `<i class="fa-solid fa-moon"></i>`
+    }
+}
+    
+    
+function filterByCategory(e){
+    Array.from(this.querySelectorAll('li')).forEach(ele=>ele.classList.remove('active'))
+    e.target.classList.add('active')
+    results.innerHTML = ''
+    websites.filter(ele=>ele.category.toLowerCase()==e.target.innerText.toLowerCase()).forEach((element)=>{
         results.appendChild(result(element.productName,element.category,element.image,element.link,element.description))
-    });
-    console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    })
 }
 
-const handleChange = (e)=>{
-     //function for input change handling
-    const ele = e.target.value.toLowerCase()
-    filtered_websites = websites.filter((i)=>(
-        i.productName.toLowerCase().includes(ele)||i.category.toLowerCase().includes(ele)||i.description.toLowerCase().includes(ele) 
-    ))
-    console.log(filtered_websites)
-    console.log(ele)
-    const results = document.querySelector('.results')
-    if(filtered_websites.length==0){
-        results.innerHTML = `<h1 class="info-heading">No website with given keyword is found</h1>`
-    }
-    else{
-        results.innerHTML = ""
-        filtered_websites.forEach(element => {
-            results.appendChild(result(element.productName,element.category,element.image,element.link,element.description))
-        });    
+async function fetchWebsites(results, categoryList) {
+    try{
+        var websites;
+        const response = await fetch('DB/product.json')
+        const data = await response.json()
+        const categories = new Set()
+            data.forEach(element => {
+              categories.add(element.category);
+              results.appendChild(result(element.productName, element.category, element.image, element.link, element.description));
+            });
+            categories.forEach(element => {
+              categoryList.innerHTML += `<li>${element}</li>`;
+            });
+            websites = data
+          return websites    
+    }catch(err){
+        // if(err.)
+        console.log(err)
     }
 }
 
-
-document.addEventListener('DOMContentLoaded',()=>{
-    const inputBar = document.querySelector('.input')
-    const results = document.querySelector('.results')
-    const themeButton = document.querySelector('#theme-btn')
-    let websites = ""
-    inputBar.addEventListener('keydown',(e)=>handleChange(e))
-    inputBar.addEventListener('keyup',(e)=>handleChange(e))
-    //reason to pass results is the function is loaded before the dom mounting 
-    fetchWebsites(results)
-    themeButton.addEventListener('click',changeTheme)
-    function changeTheme(e){
-        console.log(e)
-        e.target.innerHTML = ""
-        if(document.querySelector('body').classList.contains('dark')){
-            document.querySelector('body').classList.remove('dark')
-            e.target.innerHTML = `<i class="fa-solid fa-sun"></i>`
-        }else{
-            document.querySelector('body').classList.add('dark')
-            e.target.innerHTML = `<i class="fa-solid fa-moon"></i>`
-        }
+function handleChange(e,websites){
+    //function for input change handling
+   const ele = e.target.value.toLowerCase()
+   filtered_websites = websites.filter((i)=>(
+       i.productName.toLowerCase().includes(ele)||i.category.toLowerCase().includes(ele)||i.description.toLowerCase().includes(ele) 
+   ))
+   const results = document.querySelector('.results')
+   if(filtered_websites.length==0){
+       results.innerHTML = `<h1 class="info-heading">No website with given keyword is found</h1>`
+   }
+   else{
+       results.innerHTML = ""
+       filtered_websites.forEach(element => {
+           results.appendChild(result(element.productName,element.category,element.image,element.link,element.description))
+       });    
+   }
+}
+      
+function showCategories(e,categoryList){
+    console.log(e)
+    if(categoryList.classList.contains('active')){
+        document.querySelector('body').classList.remove('expand')
+        categoryList.classList.remove('active')
+    }else{
+        document.querySelector('body').classList.add('expand')
+        categoryList.classList.add('active')
     }
+}
 
-})
+async function main(){
+    websites = await fetchWebsites(results,categoryList)
+    inputBar.addEventListener('keydown',(e)=>handleChange(e,websites))
+    inputBar.addEventListener('keyup',(e)=>handleChange(e,websites))
+    categoryButton.addEventListener('click',(e)=>showCategories(e,categoryList))
+    categoryList.addEventListener('click',filterByCategory) 
+}
 
+main()
 
