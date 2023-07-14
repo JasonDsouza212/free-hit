95ed5774637687138d3e40800d89fd4180ba431f
Ishika <ishikakumari1906@gmail.com>
2023-07-14 11:10:56 +0530

    updated changed

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -17,15 +17,13 @@ export default function Pagination({ totalPages, atTop }) {
 	return (
 		atTop
 			? <div className="pagination-top top-pagination">
-			    <button className="firstbutton" onClick={() => handleClick(1)}><b>&lt;&lt;</b>&nbsp;</button>
-				{currentPage > 1 && <button className="prevbutton" onClick={() => handleClick(currentPage - 1)} > &lt; </button>}
+				{currentPage > 1 && <button className="nextbutton" onClick={() => handleClick(currentPage - 1)} > &lt; </button>}
 				{
 					pageNumbers.slice(currentPage - 2 >= 0 ? currentPage - 2 : 0, currentPage + 1).map(number =>
 						<button className={`paginate-button ${currentPage == number ? 'selected-page' : ''}`} key={number} onClick={() => { handleClick(number) }} >{number}</button>
 					)
 				}
 				{currentPage < totalPages && <button className="nextbutton" onClick={() => handleClick(currentPage + 1)}> &gt;</button>}
-				<button className="lastbutton" onClick={() => handleClick(totalPages)} >&nbsp;<b>&gt;&gt;</b></button>
 			</div>
 			: <div className="pagination">
 			<button className="firstbutton" onClick={() => handleClick(1)}><b>&lt;&lt;</b>&nbsp;</button>
