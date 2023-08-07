5d902448313e7d437806e9e79bf1df97b06a375f
tamannaaaaa <tamannasingh7922@gmail.com>
2023-08-07 15:33:18 +0530

    Updated the class attribute to className in the JSX code

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,18 +1,20 @@
-import "../styles/NotFound.css"
-import errorimg from "../assets/error.webp"
+import "../styles/NotFound.css";
+import errorimg from "../assets/error.webp";
 
 const NotFound = () => {
   return (
     <>
-      <div class="notfound">
-        <h1 class="err-code">404<img src={errorimg}></img></h1>
-        <p class="err-head">PAGE NOT FOUND</p>
-        <p class="err-message">The page you're looking for could not be found</p>
-        <p class="err-message">Please check the URL or Try again later</p>
-        <a class="home-link" href="/">Go to Homepage</a>
+      <div className="notfound">
+        <h1 className="err-code">
+          404<img src={errorimg} alt="Error" />
+        </h1>
+        <p className="err-head">PAGE NOT FOUND</p>
+        <p className="err-message">The page you're looking for could not be found</p>
+        <p className="err-message">Please check the URL or Try again later</p>
+        <a className="home-link" href="/">Go to Homepage</a>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
