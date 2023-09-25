6946c18339a112356b0293ccbb59bc70037a9b7f
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-09-25 09:21:47 +0530

    refactor: optimize footer code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,80 +1,68 @@
 import freehitlogo from '../assets/footer-logo.webp'
+import { NavLink } from "react-router-dom"
 import "../styles/Footer.css"
+
 const Footer = () => {
-  // gettin year dynamically
+  // getting year dynamically
   const date = new Date();
   const year = date.getFullYear();
+
+  // to add social links
+  const socialLinks = [
+    {
+      label: 'Twitter',
+      iconClass: 'socials_twitter ri-twitter-fill',
+      url: 'https://twitter.com/_Jason_Dsouza',
+    },
+    {
+      label: 'GitHub',
+      iconClass: 'socials_github ri-github-fill',
+      url: 'https://github.com/JasonDsouza212',
+    },
+    {
+      label: 'LinkedIn',
+      iconClass: 'socials_linkedin ri-linkedin-fill',
+      url: 'https://www.linkedin.com/in/jason-dsouza-130b421ba/',
+    },
+    {
+      label: 'Email',
+      iconClass: 'socials_mail ri-mail-fill',
+      url: 'mailto:jasondsouza212@gmail.com',
+    },
+  ];
   return (
     <footer>
       <div className="footer-content">
         <h3>
-          <a className="" href="/about">
-          <img className="free-logo-footer" src={freehitlogo} alt="logo" />
-            </a>
+          <NavLink to="/about">
+            <img className="free-logo-footer" src={freehitlogo} alt="logo" />
+          </NavLink>
         </h3>
-        <p>
-          Find the tools you need without breaking the bank
-        </p>
-        <p>
-          <b>Contact Us</b>
-        </p>
+        <p>Find the tools you need without breaking the bank</p>
+        <p>Contact Us</p>
         <ul className="socials">
-          <li>
-            <a 
-              aria-label="Follow me on Twitter"
-              title="Twitter (External Link)"
-              rel="noopener noreferrer"
-              target="_blank" 
-              href="https://twitter.com/_Jason_Dsouza"
-            >
-              <i className="socials_twitter ri-twitter-fill"></i>
-            </a>
-          </li>
-          <li>
-            <a 
-              aria-label="Follow me on Github"
-              title="Github (External Link)"
-              rel="noopener noreferrer"
-              target="_blank" 
-              href="https://github.com/JasonDsouza212"
-            >
-              <i className="socials_github ri-github-fill"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              aria-label="Follow me on Linkedin"
-              title="Linkedin (External Link)"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://www.linkedin.com/in/jason-dsouza-130b421ba/"
-            >
-              <i className="socials_linkedin ri-linkedin-fill"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              aria-label="Mail me on jasondsouza212@gmail.com"
-              title="Mail (External Link)"
-              rel="noopener noreferrer"
-              target="_blank" 
-              href="mailto:jasondsouza212@gmail.com"
-            >
-              <i className="socials_mail ri-mail-fill"></i>
-            </a>
-          </li>
+          {socialLinks.map((link, index) => (
+            <li key={index}>
+              <a
+                aria-label={`Follow me on ${link.label}`}
+                rel="noopener noreferrer"
+                target="_blank"
+                href={link.url}
+              >
+                <i className={link.iconClass}></i>
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="footer-bottom">
         <br />
         <p>
-          <span className='uppercase'>
-            Copyright &copy; Free-Hit {year}
-          </span>
+          <span className="uppercase">Copyright &copy; Free-Hit {year}</span>
           &nbsp;&nbsp; | &nbsp;&nbsp;
           <span>
-            Maintained by &nbsp;
-            <a target="_blank" href="https://github.com/JasonDsouza212" className='uppercase'>
+            Maintained by&nbsp;
+            <a href="https://github.com/JasonDsouza212" className="uppercase">
               <b>Jason Dsouza</b>
             </a>
           </span>
@@ -82,6 +70,7 @@ const Footer = () => {
         <br />
       </div>
     </footer>
+
   )
 }
 
